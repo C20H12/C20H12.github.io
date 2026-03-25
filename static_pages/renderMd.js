b836d934a539f8774fff88ed05d072a71c294e95
C20H12 <moowolvquackmeowao@gmail.com>
2026-03-25 16:01:14 -0400

    added dark toggle to static pages

--- static_pages/renderMd.js
+++ static_pages/renderMd.js
@@ -1,24 +1,57 @@
 import { Marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
-import * as mhl from 'https://cdn.jsdelivr.net/npm/marked-highlight@2.2.3/+esm';
-import hljs from 'https://cdn.jsdelivr.net/npm/highlight.js@11.11.1/+esm';
+import * as mhl from "https://cdn.jsdelivr.net/npm/marked-highlight@2.2.3/+esm";
+import hljs from "https://cdn.jsdelivr.net/npm/highlight.js@11.11.1/+esm";
+const HL_STYLES = {
+  light: "https://cdn.jsdelivr.net/npm/highlight.js@11.11.1/styles/atom-one-light.min.css",
+  dark: "https://cdn.jsdelivr.net/npm/highlight.js@11.11.1/styles/atom-one-dark.min.css",
+};
+
+const toggleLabel = document.createElement("label");
+const toggleInput = document.createElement("input");
+const toggleSlider = document.createElement("span");
+toggleLabel.className = "toggle";
+toggleInput.type = "checkbox";
+toggleSlider.className = "slider";
+toggleLabel.appendChild(toggleInput);
+toggleLabel.appendChild(toggleSlider);
+document.body.appendChild(toggleLabel);
 
 const style = document.createElement("link");
-style.setAttribute("rel", "stylesheet")
-style.setAttribute("href", "https://cdn.jsdelivr.net/npm/highlight.js@11.11.1/styles/1c-light.min.css")
+style.setAttribute("rel", "stylesheet");
+style.setAttribute("href", `../styles.css`);
 document.head.appendChild(style);
+
+const storedTheme = localStorage.getItem("colorTheme");
+toggleInput.checked = storedTheme === "dark";
+const themeStyle = document.createElement("link");
+themeStyle.setAttribute("rel", "stylesheet");
+themeStyle.setAttribute("href", `../styles-${storedTheme ?? "light"}.css`);
+document.head.appendChild(themeStyle);
+
+const hlstyle = document.createElement("link");
+hlstyle.setAttribute("rel", "stylesheet");
+hlstyle.setAttribute("href", HL_STYLES[storedTheme ?? "light"]);
+document.head.appendChild(hlstyle);
+
+toggleInput.addEventListener("input", () => {
+  const theme = toggleInput.checked ? "dark" : "light";
+  themeStyle.setAttribute("href", `../styles-${theme}.css`);
+  hlstyle.setAttribute("href", HL_STYLES[theme]);
+  localStorage.setItem("colorTheme", theme);
+});
 
 const contentArea = document.querySelector(".md-block");
 const path = contentArea.dataset.src;
 
 const marked = new Marked(
   mhl.markedHighlight({
-	  emptyLangClass: 'hljs',
-    langPrefix: 'hljs language-',
+    emptyLangClass: "hljs",
+    langPrefix: "hljs language-",
     highlight(code, lang, info) {
-      const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+      const language = hljs.getLanguage(lang) ? lang : "plaintext";
       return hljs.highlight(code, { language }).value;
-    }
-  })
+    },
+  }),
 );
 
 fetch(path)
